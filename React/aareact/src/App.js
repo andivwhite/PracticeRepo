aec8c5e1e79bd399e7e900686464b1ea3a1f4ebd
Andi White <andi.v.white@gmail.com>
2022-11-20 17:36:19 +0100

    test test

--- React/aareact/src/App.js
+++ React/aareact/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import FirstComponent from "./FirstComponent";
+import SecondComponent from "./SecondComponent";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <FirstComponent />
+        <SecondComponent />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
